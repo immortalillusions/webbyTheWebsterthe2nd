6d5fd34784c2f10455baa2ea6ba22acc8c519373
immortalillusions <info@example.com>
2023-07-19 11:58:31 -0400

    more

--- webster/src/pages/About.js
+++ webster/src/pages/About.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <>
         <h1>About Me</h1>
-        <p>I have qiqi c3</p>
+        <p className="transitionD">I have qiqi c3</p>
         <img src={require('./Venti.PNG')} alt="this is me" width="auto" height="450"></img>
         </>
         
--- webster/src/pages/Home.js
+++ webster/src/pages/Home.js
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
     <>
-        <h1>Hi! I'm Joanna.</h1>
-        <p>This is my first personal website.</p>
+        <h1 className="h1">Hi! I'm Joanna.</h1>
+        <p className ="p">This is my first personal website.</p>
         <p>im a noob at web dev rn but maybe ill get better</p>
         <img src={require('./tighnari_wallpaper.png')} alt="this is me" width="auto" height="450"></img>
     </>
